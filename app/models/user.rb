29f9303e4c565a7bc6832bd71f221234df97c91d
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-11-02 11:24:07 +0900

    feat: 登録してあるゴミの一覧表示する機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,29 @@ class User < ApplicationRecord
   def latest_trash
     trashes.order(created_at: :desc).first
   end
+
+  # [[燃えるゴミ, 毎週, 月曜日], [燃えないゴミ, 隔週, 木曜日], ...] のような二次配列を返す
+  def trashes_lists
+    trashes.map do |trash|
+      [
+        trash.name,
+        trash.latest_collection_day.cycle_i18n,
+        trash.latest_collection_day.day_of_week_i18n
+      ]
+    end
+  end
+
+  # ゴミ一覧表示用の文字列を整形する
+  def show_trashes
+    str = ''
+    if trashes.present?
+      trashes_lists.each do |trashes_list|
+        str << "=============================\n"
+        trashes_list.each{ |line| str << (line + "\n") }
+      end
+      str << "=============================\n"
+    else
+      str << "登録しているゴミはありません.\n"
+    end
+  end
 end
