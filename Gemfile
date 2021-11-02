0a9854edd897037cf86dd7731d0e40f239652f0f
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-11-02 10:09:28 +0900

    gem: enum_helpを導入しカラム名の日本語化を追加

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 # gem 'rack-cors'
 gem 'line-bot-api'
 gem 'dotenv-rails'
+gem 'enum_help'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/collection_day.rb
+++ app/models/collection_day.rb
@@ -1,3 +1,20 @@
 class CollectionDay < ApplicationRecord
   belongs_to :trash
+
+  enum day_of_week: {
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+    sunday: 7,
+  }
+
+  enum cycle: {
+    every_week: 1, # 毎週
+    every_other_week: 2, # 隔週
+    first_and_third: 3, # 第一・三
+    second_and_fourth: 4, # 第二・四
+  }
 end
