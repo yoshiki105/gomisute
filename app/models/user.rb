6f19362400deaf368f98c9dbbd6cd8cd622cebc3
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-11-02 10:09:28 +0900

    gem: enum_helpを導入しカラム名の日本語化を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   has_many :trashes, dependent: :destroy
 
+  enum mode: {
+    top: 0,
+    registration: 1,
+    show_all: 2,
+    edit: 3,
+    show_next: 4,
+    add_day_of_week: 5,
+    add_cycle: 6,
+  }
+
 end
