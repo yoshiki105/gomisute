da2612857109e9bfee0ed816c9774a959e15adf5
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-11-14 10:10:12 +0900

    fix: 隔週を追加.

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -3,7 +3,8 @@ class Cycle < ApplicationRecord
 
   enum name: {
     every_week: 1, # 毎週
-    first_and_third: 2, # 第1・3
-    second_and_fourth: 3, # 第2・4
+    every_other_week: 2, # 隔週
+    first_and_third: 3, # 第1・3
+    second_and_fourth: 4, # 第2・4
   }
 end
