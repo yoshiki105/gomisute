6a62c7f0c8d7ca1b97a4b5361ccd3304cbfe7164
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-11-21 10:12:56 +0900

    fix: Zeitwerk::NameErrorを修正.
    本番環境で出ていたバグ.
    class名を書いていなかったので、ロード対象になっていなかった.

--- app/models/linebot.rb
+++ app/models/linebot.rb
@@ -1,13 +1,15 @@
-require 'line/bot'
-include LinebotEvent
+class Linebot
+  require 'line/bot'
+  include LinebotEvent
 
-def respond_to_user
-  body = request.body.read
-  validate_client_signature(body)
+  def respond_to_user
+    body = request.body.read
+    validate_client_signature(body)
 
-  events = client.parse_events_from(body)
-  events.each do |event|
-    main_action(event)
+    events = client.parse_events_from(body)
+    events.each do |event|
+      main_action(event)
+    end
+    head :ok
   end
-  head :ok
 end
