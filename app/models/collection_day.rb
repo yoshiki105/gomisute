146015010db5f6d45c2136a67d3e3cb2ca921816
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-11-24 09:15:42 +0900

    fix: ゴミ編集で曜日を変更すると、他のゴミの曜日までも変更されるバグを修正.

--- app/models/collection_day.rb
+++ app/models/collection_day.rb
@@ -2,6 +2,8 @@ class CollectionDay < ApplicationRecord
   has_many :trash_collection_days, dependent: :destroy
   has_many :trashes, through: :trash_collection_days
 
+  validate :day_of_week_not_changed
+
   enum day_of_week: {
     monday: 1,
     tuesday: 2,
@@ -13,4 +15,12 @@ class CollectionDay < ApplicationRecord
   }
 
   delegate :user, to: :trash
+
+  private
+
+  def day_of_week_not_changed
+    if day_of_week_changed? && self.persisted?
+      errors.add(:day_of_week, "Change of day_of_week not allowed!")
+    end
+  end
 end
